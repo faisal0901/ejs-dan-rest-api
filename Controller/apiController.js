a03ff147134cd0b4f982d262351c4565f9714e45
faisalf0901 <muhfaisalll0402@gmail.com>
2021-02-08 04:02:12 +0700

    rest api

--- Controller/apiController.js
+++ Controller/apiController.js
@@ -1,6 +1,11 @@
 const Item = require("../Model/Item");
 const Treasure = require("../Model/Activity");
-const Traveler = require("../Model/Booking");
+const Treveler = require("../Model/Booking");
+const Category = require("../Model/Category");
+const Bank = require("../Model/Bank");
+// const Booking = require("../Model/Booking");
+// const Member = require("../Model/Member");
+
 module.exports = {
   landingPage: async (req, res) => {
     try {
@@ -8,17 +13,133 @@ module.exports = {
         .select("_id title country city price unit imageId")
         .limit(5)
         .populate({ path: "imageId", select: "_id imageUrl" });
-      const traveler = await Traveler.find();
+
+      const category = await Category.find()
+        .select("_id name")
+        .limit(3)
+        .populate({
+          path: "itemId",
+          select: "_id title country city Ispopular sumBooking imageId",
+          perDocumentLimit: 4,
+          option: { sort: { sumBooking: -1 } },
+          populate: {
+            path: "imageId",
+            select: "_id imageUrl",
+            perDocumentLimit: 1,
+          },
+        });
+
+      const treveler = await Treveler.find();
       const treasure = await Treasure.find();
       const city = await Item.find();
+
+      for (let i = 0; i < category.length; i++) {
+        for (let x = 0; x < category[i].itemId.length; x++) {
+          const item = await Item.findOne({ _id: category[i].itemId[x]._id });
+          item.Ispopular = false;
+          await item.save();
+          if (category[i].itemId[0] === category[i].itemId[x]) {
+            item.Ispopular = true;
+            await item.save();
+          }
+        }
+      }
+
+      const testimonial = {
+        _id: "asd1293uasdads1",
+        imageUrl: "images/testimonial2.jpg",
+        name: "Happy Family",
+        rate: 4.55,
+        content:
+          "What a great trip with my family and I should try again next time soon ...",
+        familyName: "Angga",
+        familyOccupation: "Product Designer",
+      };
+
       res.status(200).json({
         hero: {
-          traveler: traveler.length,
-          treasure: treasure.length,
-          city: city.length,
+          travelers: treveler.length,
+          treasures: treasure.length,
+          cities: city.length,
         },
         mostPicked,
+        category,
+        testimonial,
       });
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await Item.findOne({ _id: id })
+        .populate({
+          path: "featureId",
+          select: "_id name qty imageUrl",
+        })
+        .populate({
+          path: "activityId",
+          select: "_id name type imageUrl",
+        })
+        .populate({
+          path: "imageId",
+          select: "_id imageUrl",
+        });
+      const testimonial = {
+        _id: "asd1293uasdads1",
+        imageUrl: "images/testimonial2.jpg",
+        name: "Happy Family",
+        rate: 4.55,
+        content:
+          "What a great trip with my family and I should try again next time soon ...",
+        familyName: "Angga",
+        familyOccupation: "Product Designer",
+      };
+      const bank = await Bank.find();
+      res.status(200).json({ ...item._doc, bank, testimonial });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+  bookingPage: async (req, res) => {
+    try {
+      const {
+        idItem,
+        duration,
+        price,
+        bookingDateStart,
+        bookingDateEnd,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        accountHolder,
+        bankFrom,
+      } = req.body;
+      if (!req.file) {
+        return res.status(404).json({ message: "image not found" });
+      }
+      console.log(idItem);
+      if (
+        duration === undefined ||
+        price === undefined ||
+        bookingDateStart === undefined ||
+        bookingDateEnd === undefined ||
+        firstName === undefined ||
+        lastName === undefined ||
+        email === undefined ||
+        phoneNumber === undefined ||
+        accountHolder === undefined ||
+        bankFrom === undefined
+      ) {
+        res.status(404).json({ message: "lengkapi semua field" });
+      }
+      res.status(201).json({ message: "succes" });
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const apiController = require("../Controller/apiController");
-// const { uploadSingle, uploadMultiple } = require("../middleware/multer");
+const { uploadSingle } = require("../middleware/multer");
 const auth = require("../middleware/auth");
 router.get("/landing-page", apiController.landingPage);
+router.get("/detail-page/:id", apiController.detailPage);
+router.post("/booking-page", uploadSingle, apiController.bookingPage);
 
 module.exports = router;
